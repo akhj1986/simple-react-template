57040b468e1293b6a38410e626c092aa681a22d2
Alex Harris-Jedamski <akhj1986@gmail.com>
2019-02-05 11:31:22 +0000

    initial page setup

---
+++ src/components/MainContent/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MainContent = () => {
+  return (
+    <div className="page-container">
+      <h1> This is a Page</h1>
+    </div>
+  );
+};
+
+export default MainContent;
---
+++ src/components/NavBar/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <div className="nav-container">
+      <h2> This is a NavBar</h2>
+    </div>
+  );
+};
+
+export default NavBar;
